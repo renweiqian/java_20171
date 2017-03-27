f72575ef3fce7e84dec77f946ea5af6cb538831d
renweiqian <438433880@qq.com>
2017-03-27 16:13:16 +0800

    更新

---
+++ src/main/java/collection/ArrayTest.java
@@ -0,0 +1,29 @@
+package collection;
+
+/**
+ * Created by 任纹乾 on
+ * 2017/3/27.
+ */
+public class ArrayTest {
+    // array 数组\ [ə'reɪ]
+    public static void main(String[] args) {
+        String[] strings;
+        strings = new String[10];
+        strings[0] = "test...";
+        System.out.println(strings[0]); // 下标 索引 index
+
+        boolean[] booleans = new boolean[100];
+        System.out.println(booleans[99]); // ?
+
+        int[] ints = {1, 2, 3, 4, 5, 6, 67777, 888};
+        System.out.println(ints[6]);
+
+        double[] doubles = new double[10000];
+        System.out.println(doubles.length);
+        ArrayTest[] arrayTests = {new ArrayTest(), new ArrayTest(), new ArrayTest()};
+//        arrayTests[0] = new ArrayTest();
+        System.out.println(arrayTests[0]); // java1702.javase.collection.ArrayTest@
+        System.out.println(arrayTests[1]); // ?
+        System.out.println(arrayTests[2]); // ?
+    }
+}
