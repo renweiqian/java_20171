554f94b393fea0cd0711c34fee7df05d44af0a80
renweiqian <438433880@qq.com>
2017-03-29 14:51:03 +0800

    更新

--- src/main/java/collection/VectorTest.java
+++ src/main/java/collection/VectorTest.java
@@ -1,13 +1,13 @@
 package collection;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Vector;
 
 /**
  * Created by 任纹乾 on
  * 2017/3/29.
  */
 // vector 向量\ ['vektə]
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Vector;
 public class VectorTest {
     public static void main(String[] args) {
 
@@ -29,12 +29,21 @@ public class VectorTest {
         System.out.println(vector.capacity());
         // capacity 容量,\ 能力,\ 容积
 
+        Vector<Integer> integers = new Vector<>();
+        integers.add(1);
+        integers.add(10);
+        integers.add(100);
+        System.out.println(integers.size());
+        System.out.println(integers.capacity());
+        for (Integer integer : integers) {
+            System.out.println(integer);
+        }
+
         Set<String> strings = new HashSet<>();
         strings.add("a");
         strings.add("aa");
         strings.add("aaa");
         strings.add("aaa");
-        System.out.println(strings.size());
+//        System.out.println(strings.size());
     }
 }
-
