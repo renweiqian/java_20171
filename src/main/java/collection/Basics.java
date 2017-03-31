b37fa939da7a45aed5e7ed586b10a473e4b62c4f
renweiqian <438433880@qq.com>
2017-03-31 11:01:51 +0800

    更新

--- src/main/java/collection/Basics.java
+++ src/main/java/collection/Basics.java
@@ -4,10 +4,12 @@ package collection;  //定义包
  * Created by 任纹乾 on
  * 2017/3/31.
  */
-public class Basics {//创建的累
+public class Basics {//创建的类
     static int ONE = 1;//定义类的成员变量
     public static void main(String[] args) {//定义主方法
         String var = "Hello";//定义局部变量
+        int i = 100;//定义变量i 并赋值100
+        System.out.println(i);//在控制台输出信息
         System.out.println(ONE);//输出成员变量的值
         System.out.println(var);//输出局部变量的值
     }
