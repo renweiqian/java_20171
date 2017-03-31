97265dba16a66b00a0a2f078a7f639387fde29e1
renweiqian <438433880@qq.com>
2017-03-31 15:56:17 +0800

    更新

--- src/main/java/BasicLearning/Basics1.java
+++ src/main/java/BasicLearning/Basics1.java
@@ -15,7 +15,7 @@ public class Basics1 {
 // 而6种数值类型中有4种整数类型，另外两种是浮点类型
     boolean v = true;//布尔类型
     float  c = 0.3f;//单精度浮点数
-    double b = 0.2;//双精度浮点数
+    double b = 0.2d;//双精度浮点数
     byte x = 48,y = -108, z;//字节            -128---127
     //定义一个byte型变量型，x.y.z,并赋值给x.y
     short g = 6;//短整型         -32768---32767
@@ -29,8 +29,12 @@ public class Basics1 {
         int f = 45784612;//声明int型变量并赋值
         long g = 46789451L;//声明long型变量并赋值
         long  h = m + n + f +  g;//获得各数相加后的结果
-        System.out.println(h);//将以上变量相加的结果输出
-
+        System.out.println("四种类型相加结果："+h);//将以上变量相加的结果输出
+        float k1 = 13.23f;//定义float型变量
+        double k2 = 4562.12d;//定义double型变量
+        double k3 = 45678.1564;//定义double型变量
+        double k4 = k1 + k2 + k3;//获得各数相加后的结果
+        System.out.println("浮点相加结果："+k4);//将以上变量相加的结果输出
 
     }
 }
