c463175c422329c43b56eaecd1c7beee1ae21650
renweiqian <438433880@qq.com>
2017-04-01 08:31:46 +0800

    更新

---
+++ src/main/java/BasicLearning/Basics3.java
@@ -0,0 +1,25 @@
+package BasicLearning;
+
+import java.util.Scanner;
+
+/**
+ * Created by 任纹乾 on
+ * 2017/3/31.
+ */
+public class Basics3 {//声明类
+    public static void main(String[] args) {//主方法
+        Scanner scan = new Scanner(System.in );//创建扫描器
+        System.out.println("请输入变量A的值");
+        long A = scan.nextLong();//接受第一个变量值
+        System.out.println("请输入变量B的值");
+        long B = scan.nextLong();//接受第二个变量值
+        System.out.println("A = "+ A + "\tB ="+ B);
+        System.out.println("执行变量互换。。。");//执行变量互换
+        A = A ^ B;
+        B = B ^ A;
+        A = A ^ B;
+        System.out.println("A = "+ A + "\tB ="+ B);//输出交换的结果
+
+    }
+
+}
