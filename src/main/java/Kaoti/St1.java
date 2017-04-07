2634c94bd43f331ddfbd9753bfddf072f722c790
renweiqian <438433880@qq.com>
2017-04-07 13:40:43 +0800

    更新

--- src/main/java/Kaoti/St1.java
+++ src/main/java/Kaoti/St1.java
@@ -1,5 +1,6 @@
 package Kaoti;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -8,16 +9,28 @@ import java.util.Scanner;
  */
 public class St1{
     public static void main(String[] args) {
-        int[][] ints = new int[5][5];
+        int[][] ints = new int[2][2];
         Scanner scanner = new Scanner(System.in);
-        for (int x = 1; x < ints.length; x++) {
-            for (int y = 1; y < ints[x].length; y++) {
-                System.out.print("请输入" + x + "行" + y + "列的数：");
-                ints[x][y] = scanner.nextInt();
+        int sum = 0;
+        int mul = 1;
+        for (int i = 0; i < ints.length; i++) {
+            for (int j =0; j < ints[i].length; j++) {
+                System.out.print("请输入"+(i+1)+"行"+(j+1)+"列" );
+                ints[i][j] = scanner.nextInt();
+                if (i == j) {
+                    sum += ints[i][j];
+                }
+                if (i + j == ints.length - 1) { // ?
+                    mul *= ints[i][j];
+                }
             }
-            System.out.println();
         }
-        int n = 0;
+        System.out.println(Arrays.deepToString(ints));
+        System.out.println(sum);
+        System.out.println(mul);
+            }
+        }
+       /* int n = 0;
         for (int i = 0; i < ints.length; i++) {
             n *= ints[i][i];
         }
@@ -26,7 +39,6 @@ public class St1{
         for (int i = 0; i < ints.length; i++) {
             k *= ints[i][4 - i];
         }
-        System.out.println("副对角线上元素的乘积为：" + k);
+        System.out.println("副对角线上元素的乘积为：" + k);*/
 
-    }
-}
+
