8cbe215ec940718a12a05562cde22d51ce85c054
renweiqian <438433880@qq.com>
2017-04-11 10:18:19 +0800

    更新

---
+++ src/main/java/collection/LinkendListTest.java
@@ -0,0 +1,28 @@
+package collection;
+
+import com.sun.org.apache.xpath.internal.operations.String;
+
+import java.util.LinkedList;
+
+/**
+ * Created by 任纹乾 on
+ * 2017/4/11.
+ */
+public class LinkedListTest {
+    public static void main(String[] args) {
+        LinkedList<String> strings = new LinkedList<>();
+        strings.add("a");
+        strings.add("b");
+
+        System.out.println(strings.size());
+        System.out.println(strings.get(0));
+
+        for (String string:strings){
+            System.out.println(string);
+        }
+        System.out.println(strings.getFirst());
+        System.out.println(strings.getLast());
+
+
+    }
+}
