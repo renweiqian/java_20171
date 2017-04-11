0f7acb9ec1ac0177d48bf55f67a3ca71d1b1fe58
renweiqian <438433880@qq.com>
2017-04-11 16:16:14 +0800

    更新

--- src/main/java/collection/MyLinkdList.java
+++ src/main/java/collection/MyLinkdList.java
@@ -4,27 +4,55 @@ package collection;
  * Created by 任纹乾 on
  * 2017/4/11.
  */
-public class MyLinkdList {
-    public static void main(String[] args) {
-        MyLinkdList myLinkdList = new MyLinkdList();
-        myLinkdList.add("a");
-    }
+/*public class MyLinkdList {*/
+/*    public static void main(String[] args) {*/
+/*        MyLinkdList myLinkdList = new MyLinkdList();*/
+/*        myLinkdList.add("a");*/
+/*        myLinkdList.add("b");*/
+/*        myLinkdList.add("c");*/
+/*        System.out.println(myLinkdList.first());*/
+/*        System.out.println(myLinkdList.last());*/
+/*        System.out.println(myLinkdList.get(1));*/
+/*
+*/
 
-    private boolean add(String element) {
+/*    }*/
+/*        public String get(int index){*/
+/*            // TODO: 2017/4/11*/
+/*            return  null;*/
+/*        }*/
+/*
+*/
 
-        return  true;
-    }
-    private class Node{
-        String element;
-        Node prev;
-        Node next;
+/*
+*/
 
-        public Node(String element, Node prev, Node next) {
-            this.element = element;
-            this.prev = prev;
-            this.next = next;
+/*    private boolean add(String element) {*/
+/*        if (fist == null);*/
+/*
+*/
 
-        }
-    }
+/*        return  true;*/
+/*    }*/
+/*    private class Node{*/
+/*        String element;*/
+/*        Node prev;*/
+/*        Node next;*/
+/*
+*/
 
-}
+/*        public Node(String element, Node prev, Node next) {*/
+/*            this.element = element;*/
+/*            this.prev = prev;*/
+/*            this.next = next;*/
+/*
+*/
+
+/*        }*/
+/*    }*/
+/*
+*/
+
+/*}*/
+/*
+*/
