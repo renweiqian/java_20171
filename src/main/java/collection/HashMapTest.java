2bbe8396b049974d1d56debd763544e54180e2e7
renweiqian <438433880@qq.com>
2017-04-12 09:27:01 +0800

    更新

---
+++ src/main/java/collection/HashMapTest.java
@@ -0,0 +1,33 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by 任纹乾 on
+ * 2017/4/12.
+ */
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap<Integer,String>msp = new HashMap<>();//
+        msp.put(1,"a");
+        msp.put(100,"bcd");
+        msp.put(1000,"efgh");
+        msp.put(10000,"drga");
+
+
+
+        System.out.println(msp.size());
+        System.out.println(msp.get(100));
+        for (Integer integer:msp.keySet()){
+            System.out.println(integer+">"+msp.get(integer));
+        }
+        for (String s :msp.values()){
+            System.out.println(s);
+        }
+        for (Map.Entry<Integer,String> entry: msp.entrySet()){
+            System.out.println(entry.getKey()+">"+entry.getValue());
+        }
+
+    }
+}
