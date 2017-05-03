12b4f994668102dde7014586d884ceb9916c383e
renweiqian <438433880@qq.com>
2017-05-03 21:13:03 +0800

    更新

---
+++ src/main/java/Draft/lianxi.java
@@ -0,0 +1,11 @@
+package Draft;
+
+/**
+ * Created by 任纹乾 on
+ * 2017/5/2.
+ */
+public class lianxi {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/reflect/Animals.java
@@ -0,0 +1,19 @@
+package reflect;
+
+/**
+ * Created by 任纹乾 on
+ * 2017/5/3.
+ */
+class Animals {
+    public int age;
+    private double weight;
+
+    public Animals(){
+    }
+
+    public Animals(int age, double weight) {
+        this.age = age;
+        this.weight = weight;
+    }
+
+}
