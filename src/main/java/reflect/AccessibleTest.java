890778175a25406dcc4ff361c231e1f2dc3e01b1
renweiqian <438433880@qq.com>
2017-05-04 10:08:48 +0800

    更新

--- src/main/java/reflect/AccessibleTest.java
+++ src/main/java/reflect/AccessibleTest.java
@@ -1,13 +1,18 @@
 package reflect;
 
+import java.lang.reflect.Field;
+
 /**
  * Created by 任纹乾 on
  * 2017/5/4.
  */
 public class AccessibleTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
         Person  person = new  Person(29);
-        System.out.println(person.age);
+        Field age =Class.forName("reflect.Person").getDeclaredField("age");
+        age.setAccessible(true);
+        age.setInt(person, age.getInt(person) + 1);
+        System.out.println(age.get(person ));
     }
 }
 class Person{
