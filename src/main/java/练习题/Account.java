3b961b1dd30931984410519be9f92559451c24bd
renweiqian <438433880@qq.com>
2017-05-10 16:10:39 +0800

    更新

--- src/main/java/练习题/Account.java
+++ src/main/java/练习题/Account.java
@@ -6,32 +6,53 @@ package 练习题;
  */
 public class Account {
     private int money;
-    public void Account(int money){
+
+    public Account(int money) {
         this.money = money;
     }
-    public void  withdraw(int money){
-        if (this.money -money<0){
+
+    void withdraw(int money) {
+        System.out.println(Thread.currentThread().getName());
+        if (this.money - money < 0) {
             System.out.println("Not enough...");
             return;
         }
-        this.money-=money;
-
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.money -= money;
+        System.out.println("get the money...");
     }
-    public int getMoney(){
+
+    public int getMoney() {
         return money;
     }
 }
-class AccounTest implements Runnable{
-    private Account account;
 
+class Person implements Runnable {
+    private static Account account = new Account(1000);
 
     @Override
     public void run() {
         account.withdraw(1000);
-
     }
 
     public static void main(String[] args) {
+        Thread boy = new Thread(new Person(), "boy");
+        Thread girl = new Thread(new Person(), "girl");
 
+        boy.start();
+        girl.start();
+
+        try {
+            boy.join();
+            girl.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("money: " + account.getMoney()); // ?
     }
 }
